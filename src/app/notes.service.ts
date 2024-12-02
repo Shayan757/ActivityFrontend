54751aefbb160629f00cf3cc879d3a94b7ce93ec
Shayan Ashraf <shayanashraf295@gmail.com>
2024-12-02 18:51:23 +0500

    first commit

--- src/app/notes.service.ts
+++ src/app/notes.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class NotesService {
-  private apiUrl = 'http://localhost:5000/api/notes';
+  private apiUrl = 'https://activitybackend-production.up.railway.app/api/notes';
 
   constructor(private http: HttpClient) {}
 
